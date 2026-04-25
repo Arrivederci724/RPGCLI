6023107380882051b4be0c778f03fb14b249e6ca
Arrivederci724 <peeu123@qq.com>
2026-04-25 20:57:54 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,331 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace RPGCLI;
+
+class Get
+{
+    public int Bag;
+    private static Random random = new Random();
+    public int i = random.Next(0, 101);
+}
+
+class WuQi //武器机制
+{
+    public int defaultWuQiShh = 5;
+    public int playerImage = 100;
+    public int ManaPoints = 100;
+}
+
+class ToDownItem
+{
+    public void fall(ref int bag, WuQi wuqi)
+    {
+        Random rnd = new Random();
+        int i = rnd.Next(0,7);
+        int i2 = rnd.Next(0,6);
+        wuqi.defaultWuQiShh += i2;
+        Console.WriteLine($"增伤{i2}!");
+        Console.WriteLine($"你获得了{i}个血瓶!");
+        bag += i;
+    }
+}
+
+class Program
+{
+    bool whi2 = true;
+    int image;
+    int shh;
+    WuQi wuqi = new WuQi();
+    Random random = new Random(); //正常随机数使用
+    bool whi = true;
+    Get get = new Get();
+
+    private void ZhanDou(ref int playerImage, ref int defaultWuQiShh)
+    {
+        whi = true;
+        ToDownItem toDownItem = new ToDownItem();
+            if (whi)
+            {
+                Get get1 = new Get();
+                if (get1.i == 50)
+                {
+                    image = random.Next(200,411);
+                    shh = random.Next(0,41);
+                }
+                else
+                {
+                    image = random.Next(0, 101); //血量
+                    shh = random.Next(0, 21); //伤害
+                }
+
+                Console.WriteLine("开始战斗");
+            }
+
+            while (whi)
+            {
+                Console.WriteLine($"玩家血量:{playerImage}\n玩家伤害:{defaultWuQiShh}\n怪物血量:{image}\n怪物伤害{shh}\n");
+                Console.WriteLine("1.战斗,2.跳过,3.逃跑,4.血瓶,5.技能");
+                try
+                {
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    switch (a)
+                    {
+                        case 1:
+                            image -= defaultWuQiShh; //怪物掉血机制
+                            if (image <= 0) //怪物死亡机制
+                            {
+                                Console.WriteLine("你赢了");
+                                toDownItem.fall(ref get.Bag, wuqi);
+                                whi = false;
+                                whi2 = true;
+                            }
+
+                            playerImage -= shh;
+                            if (playerImage <= 0) //玩家死亡机制 同下
+                            {
+                                string s = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:{Convert.ToString(wuqi.playerImage / 2)}|背包:{get.Bag}";
+                                Console.WriteLine("你死了");
+                                File.WriteAllText("s.sav", s);
+                                Environment.Exit(0);
+                            }
+
+                            break;
+                        case 2:
+                            playerImage -= shh;
+                            if (playerImage <= 0)
+                            {
+                                string s = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:{Convert.ToString(wuqi.playerImage / 2)}|背包:{get.Bag}";
+                                Console.WriteLine("你死了");
+                                File.WriteAllText("s.sav", s);
+                                Environment.Exit(0);
+                            }
+
+                            break;
+                        case 3:
+
+                            bool TryEscape()
+                            {
+                                int chance = random.Next(0, 100);
+                                return chance < 25;
+                            }
+
+                            if (TryEscape())
+                            {
+                                Console.WriteLine("逃跑成功");
+                                whi = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine("失败");
+                                playerImage -= shh;
+                                if (playerImage <= 0)
+                                {
+                                    string s = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:{Convert.ToString(wuqi.playerImage / 2)}|背包:{string.Join(",", get.Bag)}";
+                                    Console.WriteLine("你死了");
+                                    File.WriteAllText("s.sav", s);
+                                    Environment.Exit(0);
+                                }
+
+                            }
+                                                                                                                        
+                                                                                                                        
+                            break;
+                            case 4:
+                            Console.WriteLine(get.Bag);
+                            wuqi.playerImage += 50;
+                            Console.WriteLine("使用成功");
+                            get.Bag--;
+                            break;
+                        case 5:
+                            if (wuqi.ManaPoints >= 20)
+                            {
+                                Console.WriteLine("使用成功");
+                                image -= 35;
+                                if (image <= 0)
+                                {
+                                    Console.WriteLine("你赢了");
+                                    toDownItem.fall(ref get.Bag, wuqi);
+                                    whi = false;
+                                    whi2 = true;
+                                }
+                                wuqi.ManaPoints -= 20;
+                            }
+                            else
+                            {
+                                Console.WriteLine("法力值不足");
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("没有这个选项");
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Environment.Exit(0);
+                }
+            }
+    }
+
+    [SuppressMessage("ReSharper", "FunctionNeverReturns")]
+    private void main()
+    { 
+        if (File.Exists("s.sav")) 
+        {
+            Console.WriteLine("1.继续游戏,2.退出");
+            try
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                switch (n) 
+                { 
+                    case 1:
+                        string save = File.ReadAllText("s.sav");
+                        string[] saveBefore = save.Split('|');
+                        string[] saveBefore2 = saveBefore[0].Split(':');
+                        wuqi.defaultWuQiShh = Convert.ToInt32(saveBefore2[1]);
+                        string[] saveBefore3 = saveBefore[1].Split(':');
+                        wuqi.playerImage = Convert.ToInt32(saveBefore3[1]);
+                        string[] saveBefore4 = saveBefore[2].Split(':');
+                        get.Bag = Convert.ToInt32(saveBefore4[1]);
+
+                        break;
+                          case 2:
+                              Console.WriteLine("再见!");
+                              string s = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:{Convert.ToString(wuqi.playerImage)}|背包:{get.Bag}";
+                              File.WriteAllText("s.sav", s);
+                              Thread.Sleep(2000);
+                              Environment.Exit(0);
+                              break;
+                          default:
+                              Console.WriteLine("没有这个选项");
+                              break;
+                      }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(0);
+            }
+        }
+        else
+        {
+            Console.WriteLine("1.开始游戏,2.退出");
+            try
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                switch (n)
+                {
+                    case 1:
+                        whi2 = true;
+                        break;
+                    case 2:
+                        Console.WriteLine("再见!");
+                        Thread.Sleep(2000);
+                        break;
+                    default:
+                        Console.WriteLine("没有这个选项");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        while (true)
+        {
+            while (whi2)
+            {
+                Console.WriteLine("1.前进,2.道具,3.休息,4.查看,5.退出");
+                try
+                {
+                    int n2 = Convert.ToInt32(Console.ReadLine());
+                    switch (n2)
+                    {
+                        case 1:
+                            int i = random.Next(0, 101);
+                            if (i <= 30)
+                            {
+                                whi2 = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine("无事发生");
+                            }
+                            break;
+                        case 2:
+                            if (get.Bag == 0)
+                            {
+                                Console.WriteLine("没有血瓶");
+                            }
+                            else
+                            {
+                                get.Bag--;
+                                wuqi.playerImage += 50;
+                                Console.WriteLine("使用成功");
+                                int i2 = random.Next(0, 101);
+                                if (i2 <= 30)
+                                {
+                                    whi2 = false;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("无事发生");
+                                }
+                            }
+
+                            break;
+                        case 3:
+                            Console.WriteLine("已休息");
+                            if (wuqi.ManaPoints <= 100)
+                            {
+                                wuqi.ManaPoints = Math.Min(wuqi.ManaPoints + 35, 100);
+                                int i3 = random.Next(0, 101);
+                                if (i3 <= 30)
+                                {
+                                    whi2 = false;
+                                }
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine($"伤害:{wuqi.defaultWuQiShh},血量:{wuqi.playerImage},法力值:{wuqi.ManaPoints},背包:{get.Bag}");
+                            break;
+                        case 5:
+                            Console.WriteLine("再见!");
+                            string s2 = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:{Convert.ToString(wuqi.playerImage)}|背包:{get.Bag}";
+                            File.WriteAllText("s.sav", s2);
+                            Thread.Sleep(2000);
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("没有这个选项");
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Environment.Exit(0);
+                }
+            }
+            whi2 = true;
+            int Explore = random.Next(0, 101);
+            if (Explore > 50)
+            {
+                ZhanDou(ref wuqi.playerImage, ref wuqi.defaultWuQiShh);
+                Thread.Sleep(1500);
+            }
+            else if (Explore == 50)
+            {
+                ZhanDou(ref wuqi.playerImage, ref wuqi.defaultWuQiShh);
+            }
+        }
+    }
+
+    public static void Main()
+    {
+        Program p = new Program();
+        p.main();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# RPGCLI
-RPGCLI is a command-line role-playing game built entirely for terminal. This is my first open-source project on GitHub. A GUI version may be developed in the future, though not guaranteed.Just for fun.
+# 控制台 RPG 游戏
+
+一个用 C# 编写的轻量级控制台角色扮演游戏。
+
+##  功能特色
+- 回合制战斗系统
+- 随机探索与遭遇
+- 属性与道具管理
+- 自动存档/读档
+
+##  快速开始
+##  基本玩法
+1. **探索阶段**：前进、使用道具、休息、查看状态
+2. **战斗阶段**：攻击、防御、逃跑、使用技能
+3. **属性**：生命值、法力值、武器伤害
+4. **道具**：血瓶可恢复生命值
+
+##  存档说明
+进度自动保存到 `s.sav` 文件，下次启动可选"继续游戏"。
+
+##  项目结构
+- `Program.cs` - 主程序与游戏逻辑
+- 使用 `WuQi`、`Get` 等类管理游戏状态
+## 注意事项
+由于项目完完全全是CLI构造 所以暂且不能打包成exe 也就是说 需要玩家主动下载.NET10平台 或者修改csproj到你的.NET版本
+修改<TargetFrame>标签内的版本 然后在终端进入项目文件夹 运行dotnet run 我清楚这体验是不好的 但我没有办法 我会继续尝试优化的
+
+# Console RPG Game
+
+A lightweight console role-playing game written in C#.
+
+## Features
+- Turn-based combat system
+- Random exploration and encounters
+- Attribute and item management
+- Automatic save/load
+
+##  Quick Start
+##  Basic Gameplay
+1. **Exploration Phase**: Move forward, use items, rest, check status
+2. **Combat Phase**: Attack, defend, escape, use skills
+3. **Attributes**: Health, mana, weapon damage
+4. **Items**: Health potions restore HP
+
+##  Save System
+Progress is automatically saved to the `s.sav` file. Choose "Continue Game" on the next launch.
+
+##  Project Structure
+- `Program.cs` - Main program and game logic
+- Uses classes like `WuQi`, `Get` to manage game state
+## Notes
+Since the project is entirely CLI-based, it currently cannot be packaged as an executable. This means players need to:
+1. Actively download the .NET 10 platform, or
+2. Modify the csproj file to match your .NET version by adjusting the version inside the `<TargetFramework>` tag.
+
+Then, open a terminal in the project folder and run `dotnet run`.
+
+I understand this experience is not ideal, but I have no other option at the moment. I will continue to work on optimizations.
