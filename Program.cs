c4bd6b44e2d4c5092842bfcb672cb07215bd9720
Arrice <peeu123@qq.com>
2026-05-09 23:12:41 +0800

    增加商店 金币等机制 需重初始化存档

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ namespace RPGCLI;
 class Get
 {
     public int Bag;
+    public int Image;
+    public int ManaBag;
+    public int Count;
     private static Random random = new Random();
     public int i = random.Next(0, 101);
 }
@@ -16,16 +19,93 @@ class WuQi //武器机制
     public int ManaPoints = 100;
 }
 
+class Shop
+{
+    public Shop()
+    {
+        Console.WriteLine("血瓶:10元\n增伤药水:30元\n法力值药水:55元\n4.退出");
+        try
+        {
+            Get get = new Get();
+            int n = Convert.ToInt32(Console.ReadLine());
+            bool i = true;
+            while (i)
+            {
+                switch (n)
+                {
+                    case 1:
+                        if (get.Count >= 10)
+                        {
+                            get.Count -= 10;
+                            get.Bag++;
+                            Console.WriteLine("购买成功");
+                        }
+                        else
+                        {
+                            Console.WriteLine("购买失败 金币不足");
+                            return;
+                        }
+
+                        break;
+                    case 2:
+                        if (get.Count >= 30)
+                        {
+                            get.Count -= 30;
+                            get.Image++;
+                            Console.WriteLine("购买成功");
+                        }
+                        else
+                        {
+                            Console.WriteLine("购买失败 金币不足");
+	            return;
+                        }
+
+                        break;
+                    case 3:
+                        if (get.Count >= 55)
+                        {
+                            get.Count -= 55;
+                            get.ManaBag++;
+                            Console.WriteLine("购买成功");
+                        }
+                        else
+                        {
+                            Console.WriteLine("购买失败 金币不足");
+ 	            return;
+                        }
+
+                        break;
+                    case 4:
+                        i = false;
+                        break;
+                    default:
+                        Console.WriteLine("没有这个选项");
+                        break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
+
 class ToDownItem
 {
     public void fall(ref int bag, WuQi wuqi)
     {
+        Get get = new Get();
+        double d = Convert.ToDouble(wuqi.playerImage);
         Random rnd = new Random();
-        int i = rnd.Next(0,7);
-        int i2 = rnd.Next(0,6);
+        int i = rnd.Next(0,(int)(get.Bag*0.1));
+        int i2 = rnd.Next(0,(int)(wuqi.defaultWuQiShh*0.1));
+        int i3 = rnd.Next(0, (int)(d*0.25));
         wuqi.defaultWuQiShh += i2;
         Console.WriteLine($"增伤{i2}!");
         Console.WriteLine($"你获得了{i}个血瓶!");
+        Console.WriteLine($"你获得了{i3}个金币!");
+        get.Count += i3;
         bag += i;
     }
 }
@@ -62,8 +142,8 @@ class Program
 
             while (whi)
             {
-                Console.WriteLine($"玩家血量:{playerImage}\n玩家伤害:{defaultWuQiShh}\n怪物血量:{image}\n怪物伤害{shh}\n");
-                Console.WriteLine("1.战斗,2.跳过,3.逃跑,4.血瓶,5.技能");
+                Console.WriteLine($"玩家血量:{playerImage}\n玩家伤害:{defaultWuQiShh}\n法力值:{wuqi.ManaPoints}\n怪物血量:{image}\n怪物伤害{shh}\n");
+                Console.WriteLine("1.战斗,2.跳过,3.逃跑,4.血瓶,5.技能,6.回法,7.增伤");
                 try
                 {
                     int a = Convert.ToInt32(Console.ReadLine());
@@ -82,7 +162,7 @@ class Program
                             playerImage -= shh;
                             if (playerImage <= 0) //玩家死亡机制 同下
                             {
-                                string s = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:50|背包:{get.Bag}|法力值:{wuqi.ManaPoints}";
+                                string s = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:50|背包:{get.Bag}|法力值:{wuqi.ManaPoints}|金币:{get.Count}|增伤药水:{get.Image}|回法药水:{get.ManaBag}";
                                 Console.WriteLine("你死了");
                                 File.WriteAllText("s.sav", s);
                                 Environment.Exit(0);
@@ -93,7 +173,7 @@ class Program
                             playerImage -= shh;
                             if (playerImage <= 0)
                             {
-                                string s = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:50|背包:{get.Bag}|法力值:{wuqi.ManaPoints}";
+                                string s = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:50|背包:{get.Bag}|法力值:{wuqi.ManaPoints}|金币:{get.Count}|增伤药水:{get.Image}|回法药水:{get.ManaBag}";
                                 Console.WriteLine("你死了");
                                 File.WriteAllText("s.sav", s);
                                 Environment.Exit(0);
@@ -119,7 +199,7 @@ class Program
                                 playerImage -= shh;
                                 if (playerImage <= 0)
                                 {
-                                    string s = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:50|背包:{get.Bag}|法力值:{wuqi.ManaPoints}";
+                                    string s = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:50|背包:{get.Bag}|法力值:{wuqi.ManaPoints}|金币:{get.Count}|增伤药水:{get.Image}|回法药水:{get.ManaBag}";
                                     Console.WriteLine("你死了");
                                     File.WriteAllText("s.sav", s);
                                     Environment.Exit(0);
@@ -159,6 +239,32 @@ class Program
                                 Console.WriteLine("法力值不足");
                             }
                             break;
+                        case 6:
+                            if (get.ManaBag > 0)
+                            {
+                                get.ManaBag--;
+                                wuqi.ManaPoints = Math.Min(wuqi.ManaPoints + 35,100);
+                                Console.WriteLine("使用成功");
+                            }
+                            else
+                            {
+                                Console.WriteLine("使用失败 物品不足");
+                            }
+
+                            break;
+                        case 7:
+                            if (get.Image > 0)
+                            {
+                                get.Image--;
+                                wuqi.playerImage += 50;
+                                Console.WriteLine("使用成功");
+                            }
+                            else
+                            {
+                                Console.WriteLine("使用失败 物品不足");
+                            }
+
+                            break;
                         default:
                             Console.WriteLine("没有这个选项");
                             break;
@@ -191,13 +297,19 @@ class Program
                         wuqi.playerImage = Convert.ToInt32(saveBefore3[1]);
                         string[] saveBefore4 = saveBefore[2].Split(':');
                         string[] saveBefore5 = saveBefore[3].Split(':');
+                        string[] saveBefore6 = saveBefore[4].Split(':');
+                        string[] saveBefore7 = saveBefore[5].Split(':');
+                        string[] saveBefore8 = saveBefore[6].Split(':');
                         get.Bag = Convert.ToInt32(saveBefore4[1]);
                         wuqi.ManaPoints = Convert.ToInt32(saveBefore5[1]);
+                        get.Count =  Convert.ToInt32(saveBefore6[1]);
+                        get.Image = Convert.ToInt32(saveBefore7[1]);
+                        get.ManaBag = Convert.ToInt32(saveBefore8[1]);
 
                         break;
                           case 2:
                               Console.WriteLine("再见!");
-                              string s = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:{Convert.ToString(wuqi.playerImage)}|背包:{get.Bag}|法力值:{wuqi.ManaPoints}";
+                              string s = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:{Convert.ToString(wuqi.playerImage)}|背包:{get.Bag}|法力值:{wuqi.ManaPoints}|金币:{get.Count}|增伤药水:{get.Image}|回法药水:{get.ManaBag}";
                               File.WriteAllText("s.sav", s);
                               Thread.Sleep(2000);
                               Environment.Exit(0);
@@ -241,7 +353,7 @@ class Program
         {
             while (whi2)
             {
-                Console.WriteLine("1.前进,2.血瓶,3.休息,4.查看,5.退出");
+                Console.WriteLine("1.前进,2.血瓶,3.休息,4.查看,5,增伤,6.商店,7.退出");
                 try
                 {
                     int n2 = Convert.ToInt32(Console.ReadLine());
@@ -293,11 +405,28 @@ class Program
                             }
                             break;
                         case 4:
-                            Console.WriteLine($"伤害:{wuqi.defaultWuQiShh},血量:{wuqi.playerImage},法力值:{wuqi.ManaPoints},背包:{get.Bag}");
+                            Console.WriteLine($"伤害:{wuqi.defaultWuQiShh},血量:{wuqi.playerImage},法力值:{wuqi.ManaPoints},背包:{get.Bag},金币:{get.Count},增伤药水:{get.Image},回法药水:{get.ManaBag}");
                             break;
                         case 5:
+                            
+                            if (get.Image > 0)
+                            {
+                                get.Image--;
+                                wuqi.defaultWuQiShh += 3;
+                                Console.WriteLine("使用成功");
+                            }
+                            else
+                            {
+                                Console.WriteLine("使用失败 物品不足");
+                            }
+
+                            break;
+                        case 6:
+                            new Shop();
+                            break;
+                        case 7:
                             Console.WriteLine("再见!");
-                            string s2 = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:{Convert.ToString(wuqi.playerImage)}|背包:{get.Bag}|法力值:{wuqi.ManaPoints}";
+                            string s2 = $"伤害:{Convert.ToString(wuqi.defaultWuQiShh)}|血量:{Convert.ToString(wuqi.playerImage)}|背包:{get.Bag}|法力值:{wuqi.ManaPoints}|金币:{get.Count}|增伤药水:{get.Image}|回法药水:{get.ManaBag}";
                             File.WriteAllText("s.sav", s2);
                             Thread.Sleep(2000);
                             Environment.Exit(0);
--- obj/Debug/net10.0/RPGCLI.AssemblyInfo.cs
+++ obj/Debug/net10.0/RPGCLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGCLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68d3a01c936536bf3c3cd926536573a5c9e411d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51c1ce909d5f5cf5b83484c6b5406d3b05d4555e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGCLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGCLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
